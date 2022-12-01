82dab8f4e2a6993703a3d1a85ae4a0a51d334304
ADMIN <Bharath Alwala>
2022-12-01 22:26:06 +0530

    Custom Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup ,FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Validations';
+  title = 'Angular Form Validation Tutorial';
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+   this.createForm();
+}
+
+createForm() {
+  this.angForm = this.fb.group({
+     name: ['', Validators.required ],
+    //  phone: ['', [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")] ]
+    phone: ['', [Validators.required,this.validatePhone()] ]
+  });
+}
+
+validatePhone(): {[key: string]: any} | null  {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    var regex = /[a-zA-Z]/g;
+    var phone = control.value.replace( /^\D+/g, '');
+      debugger;
+    if (phone.length < 10 || regex.test(control.value)) {
+      return { 'phoneNumberInvalid': true };
+    }
+    
+    return null;
+  }
+}
+
+
 }
